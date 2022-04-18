74f197f67cb329e7a76922ceecb2e7beb5f4e0a7
Mamad Shahib <41083095+mahadshahib@users.noreply.github.com>
2022-04-18 15:44:38 +0430

    Initial Commit

---
+++ MofidCodingChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MofidCodingChallange
+//
+//  Created by Mamad Shahib on 1/29/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
